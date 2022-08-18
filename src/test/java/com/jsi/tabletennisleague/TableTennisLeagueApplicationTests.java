9866d78c699462a158ec542774cdcf0847dadcbe
jleiner <jleiner@pfscm.org>
2022-08-18 12:05:40 -0400

    Initial Setup

---
+++ src/test/java/com/jsi/tabletennisleague/TableTennisLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsi.tabletennisleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
